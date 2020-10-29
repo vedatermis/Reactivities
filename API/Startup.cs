5f2f1c4306d08c619faecdbc6fc5f345e607c9ef
Vedat ERMİŞ <vedatermis@hotmail.com>
2020-10-29 10:15:48 +0300

    commit

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Persistence;
 
 namespace API
 {
@@ -18,6 +20,11 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(opt => 
+            { 
+                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            
             services.AddControllers();
         }
 
