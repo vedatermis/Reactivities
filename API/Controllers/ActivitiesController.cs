e8ef681a1ea4fd0d3dc51e0cde560ddf33aa13c8
Vedat ERMİŞ <vedatermis@hotmail.com>
2020-10-30 19:24:31 +0300

    commit

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Activities;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController: ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ActivitiesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> List(CancellationToken cancellationToken)
+        {
+            return await _mediator.Send(new List.Query(), cancellationToken);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> Details(Guid id)
+        {
+            return await _mediator.Send(new Details.Query {Id = id});
+        }
+    }
+}
